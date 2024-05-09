d1fe0cbc5defbb4a47458bdd75dccdceed709825
R00t <100488601+MrShellcode@users.noreply.github.com>
2024-05-10 02:33:51 +0330

    MacChanger.py

--- MacChanger.py
+++ MacChanger.py
@@ -3,21 +3,24 @@ import re
 import os
 import sys
 import random
+import netifaces
 
 def check_root_privileges():
     if os.geteuid() != 0:
         print("\nError: Only root can run this script.\n")
         sys.exit(1)  # Exit with a non-zero status code
 
+def generate_random_mac():
+    # Generate a random MAC address
+    random_mac = ":".join(f"{random.randint(0, 255):02X}" for _ in range(6))
+    return random_mac
+    
 def validate_mac(mac):
     # Regular expression to validate MAC address format
     pattern = r"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$"
     return re.match(pattern, mac)
 
-def generate_random_mac():
-    # Generate a random MAC address
-    random_mac = ":".join(f"{random.randint(0, 255):02X}" for _ in range(6))
-    return random_mac
+
 
 def change_mac(interface, new_mac):
     try:
@@ -45,12 +48,13 @@ def change_mac(interface, new_mac):
 
 if __name__ == "__main__":
     check_root_privileges()
+    
+    interface_list = netifaces.interfaces()
+    print("Your System Interface is : ",interface_list)
     interface = input("Enter the network interface name (e.g., eth0): ")
     new_mac = input("Enter the new MAC address (format: XX:XX:XX:XX:XX:XX or 'random'): ")
-
     if new_mac.lower() == "random":
         print("Generating a random MAC address...")
         new_mac = generate_random_mac()
-
-    print("You entered:", new_mac)
+    print("Your New Mac Adress is ", new_mac)
     change_mac(interface, new_mac)

9ef8b2537f0007d3503544aa5ad54d2c5a8aeb02
R00t <100488601+MrShellcode@users.noreply.github.com>
2024-06-01 11:50:25 +0330

    Update MacChanger.py

--- MacChanger.py
+++ MacChanger.py
@@ -1,60 +1,46 @@
+#! /usr/bin/env python
+
 import subprocess
+import optparse
 import re
-import os
-import sys
-import random
-import netifaces
 
-def check_root_privileges():
-    if os.geteuid() != 0:
-        print("\nError: Only root can run this script.\n")
-        sys.exit(1)  # Exit with a non-zero status code
 
-def generate_random_mac():
-    # Generate a random MAC address
-    random_mac = ":".join(f"{random.randint(0, 255):02X}" for _ in range(6))
-    return random_mac
-    
-def validate_mac(mac):
-    # Regular expression to validate MAC address format
-    pattern = r"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$"
-    return re.match(pattern, mac)
-
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New Mac Address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface or not options.new_mac:
+    parser.error("[-] Please specify both the interface and the new MAC address; use --help for more info")
+    elif not options.interface:
+        parser.error("[-] Please specify the interface; use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify the new MAC address; use --help for more info")
+    return options
 
 
 def change_mac(interface, new_mac):
-    try:
-        if new_mac.lower() == "random":
-            new_mac = generate_random_mac()
-            print(f"Generated random MAC address: {new_mac}")
+    print("[+] Changing MAC address of " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-        # Validate the new MAC address
-        if not validate_mac(new_mac):
-            print("Invalid MAC address format. Please use XX:XX:XX:XX:XX:XX.")
-            return
 
-        # Shut down the interface
-        subprocess.run(["ifconfig", interface, "down"])
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface]).decode("utf-8")
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("Error: Could not Read MAC address.")
 
-        # Change the interface's hardware address
-        subprocess.run(["ifconfig", interface, "hw", "ether", new_mac])
 
-        # Bring the interface back up
-        subprocess.run(["ifconfig", interface, "up"])
-
-        print(f"MAC address changed to {new_mac} for {interface}")
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e}")
-
-if __name__ == "__main__":
-    check_root_privileges()
-    
-    interface_list = netifaces.interfaces()
-    print("Your System Interface is : ",interface_list)
-    interface = input("Enter the network interface name (e.g., eth0): ")
-    new_mac = input("Enter the new MAC address (format: XX:XX:XX:XX:XX:XX or 'random'): ")
-    if new_mac.lower() == "random":
-        print("Generating a random MAC address...")
-        new_mac = generate_random_mac()
-    print("Your New Mac Adress is ", new_mac)
-    change_mac(interface, new_mac)
+options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC is " + str(current_mac))
+change_mac(options.interface, options.new_mac)
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] The Mac Address Successfully Changed to " + current_mac)
+else:
+    print("[-] The Mac Address didn't get changed ")
